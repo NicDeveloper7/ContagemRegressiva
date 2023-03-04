b787d7a9a11a8d6a24abbe05d4763740825bae8d
nicholasdev@zohomail.com <nicholasdev@zohomail.com>
2023-03-03 22:35:20 -0300

    Contagem regressiva em csharp

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("---Contagem Regressiva---");
+Console.WriteLine("Preparar...");
+Console.ReadLine();
+Console.WriteLine("Apontar...");
+Console.ReadLine();
+Console.WriteLine("FOGO!");
